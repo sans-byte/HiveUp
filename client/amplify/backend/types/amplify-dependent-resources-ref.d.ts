c2d69ff8753b5cc6094abee6b86a6ed84dffa4db
sanskar.jain <sanskar.jain@accenture.com>
2024-03-29 00:11:46 +0530

    amplify setup for auth

---
+++ client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "hiveupf0146024": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="App">
+      <Authenticator>
+        {({ signOut }) => (
+          <main>
+            <header className="App-header">
+              <button
+                onClick={signOut}
+                style={{
+                  margin: "20px",
+                  fontSize: "0.8rem",
+                  padding: "5px 10px",
+                  marginTop: "20px",
+                }}
+              >
+                Sign Out
+              </button>
+            </header>
+          </main>
+        )}
+      </Authenticator>
+    </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
